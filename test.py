57c216716275d0c44fc0609ffa1e9472a901ce25
João Barbosa <bmpj13@hotmail.com>
2019-10-27 01:18:10 +0100

    iterating by each target to apply hough

--- test.py
+++ test.py
@@ -1,5 +1,41 @@
 import numpy as np
 import cv2 as cv
+from math import ceil
+
+def rotate_image(mat, angle):
+    """
+    Rotates an image (angle in degrees) and expands image to avoid cropping
+    """
+    height, width = mat.shape[:2] # image shape has 3 dimensions
+    image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
+
+    rotation_mat = cv.getRotationMatrix2D(image_center, angle, 1.)
+
+    # rotation calculates the cos and sin, taking absolutes of those.
+    abs_cos = abs(rotation_mat[0,0]) 
+    abs_sin = abs(rotation_mat[0,1])
+
+    # find the new width and height bounds
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+
+    # subtract old image center (bringing image back to origo) and adding the new image center coordinates
+    rotation_mat[0, 2] += bound_w/2 - image_center[0]
+    rotation_mat[1, 2] += bound_h/2 - image_center[1]
+
+    # rotate image with the new bounds and translated rotation matrix
+    rotated_mat = cv.warpAffine(mat, rotation_mat, (bound_w, bound_h))
+    return rotated_mat
+
+def get_box_values(box):
+    (x, y), (width, height), angle = box
+    x, y, width, height = x, y, width, height
+
+    if angle < -45.0:     # Switch height and width if angle between -90 and -45 ( width, height 90º = height, width 0º )
+        width, height = height, width
+
+    return x, y, width, height, angle
+
 
 # Apply CLAHE to colored image by converting to LAB colorspace
 def applyCLAHE(img, clipLimit, tileGridSize):
@@ -13,7 +49,7 @@ def applyCLAHE(img, clipLimit, tileGridSize):
 
     return img_clahe
 
-def removeBadContours(img, minHeight = 25, maxHeight = 350, minRatio = 1.5):
+def removeBadContours(img, minHeight = 20, maxHeight = 350, minRatio = 2):
     img = img.copy()
     img_colored = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
     mask = np.zeros(img.shape[:2], dtype="uint8")
@@ -21,46 +57,41 @@ def removeBadContours(img, minHeight = 25, maxHeight = 350, minRatio = 1.5):
     cnts, _ = cv.findContours(img, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     contours = sorted(cnts, key=cv.contourArea, reverse=True)
     for contour in contours:
-        x,y,w,h = cv.boundingRect(contour)
+        rbox = cv.minAreaRect(contour)
+        pts = cv.boxPoints(rbox).astype(np.int32)
+        x, y, width, height, _ = get_box_values(rbox)
 
-        if w*h < minHeight:     # exit loop if area is less than minHeight
+        if width * height < minHeight:     # exit loop if area is less than minHeight
             break
-        elif h < minHeight or h > maxHeight or (h*1.0/w) < minRatio:    # if contour is bad, skip
+        elif height < minHeight or height > maxHeight or (height * 1.0 / width) < minRatio:    # if contour is bad, skip
             continue
         else:
             cv.drawContours(mask, [contour], -1, 1, -1)
-            cv.rectangle(img_colored, (x,y), (x+w,y+h), (0,0,255), 2)
+            cv.drawContours(img_colored, [pts], -1, (0, 255, 0), 1, cv.LINE_AA)
 
         img_filtered_contours = cv.bitwise_and(img, img, mask=mask)
 
     return img_filtered_contours, img_colored, mask
 
-def removeBadTargets(img, minArea = 2000, maxRatioHorizontal = 3, maxRatioVertical = 2.5):
+def removeBadTargets(img, minArea = 2000, maxRatioHorizontal = 3, maxRatioVertical = 2):
     img = img.copy()
     img_colored = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
     mask = np.zeros(img.shape[:2], dtype="uint8")
 
     cnts, _ = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     contours = sorted(cnts, key=cv.contourArea, reverse=True)
-
     for contour in contours:
         rbox = cv.minAreaRect(contour)
-        (x, y), (width, height), angle = rbox
-        x,y,width,height = int(x), int(y), int(width), int(height)
         pts = cv.boxPoints(rbox).astype(np.int32)
+        x, y, width, height, _ = get_box_values(rbox)
 
-        if -90.0 <= angle <= -45.0:     # Switch height and width if angle between -90 and -45 to keep logic below
-            width,height = height,width
-
-        if width*height < minArea:
-            cv.drawContours(mask, [pts], -1, 0, -1)
-            cv.drawContours(img_colored, [pts], -1, (0, 255, 0), 1, cv.LINE_AA)
-        elif width*1.0/height > maxRatioHorizontal or height*1.0/width > maxRatioVertical:
-            cv.drawContours(mask, [pts], -1, 0, -1)
-            cv.drawContours(img_colored, [pts], -1, (255, 0, 0), 1, cv.LINE_AA)
+        if width * height < minArea:
+            break
+        elif width * 1.0 / height > maxRatioHorizontal or height * 1.0 / width > maxRatioVertical:
+            continue
         else:
             cv.drawContours(mask, [pts], -1, 1, -1)
-            cv.drawContours(img_colored, [pts], -1, (0, 0, 255), 1, cv.LINE_AA)
+            cv.drawContours(img_colored, [pts], -1, (0, 255, 0), 1, cv.LINE_AA)
 
     img_filtered_targets = cv.bitwise_and(img, img, mask=mask)
 
@@ -82,28 +113,34 @@ for i in range(0, 26):
     img_filtered_contours, _, _ = removeBadContours(img_thresh)
 
     # Close black bars
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (31, 1))
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (31, 11))
     img_closed = cv.morphologyEx(img_filtered_contours, cv.MORPH_CLOSE, kernel)
 
-    img_filtered_targets, img_colored, _ = removeBadTargets(img_closed)
+    img_filtered_targets, _, _ = removeBadTargets(img_closed)
 
+    cv.imshow('Original', img)
     cv.imshow('CLAHE', img_clahe)
     cv.imshow('Gray', img_gray)
-    cv.imshow('Thresh', img_thresh)
-    cv.imshow('Filtered Contours', img_filtered_contours)
+    cv.imshow('Threshold', img_thresh)
+    cv.imshow('Countours Filter', img_filtered_contours)
     cv.imshow('Closed', img_closed)
-    cv.imshow('Filtered targets', img_filtered_targets)
-    cv.imshow('Colored', img_colored)
-
-    img_filtered_contours_copy = img_filtered_contours.copy()
+    cv.imshow('Targets Filter', img_filtered_targets)
+    
     cnts, _ = cv.findContours(img_filtered_targets, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     contours = sorted(cnts, key=cv.contourArea, reverse=True)
+    print()
     for contour in contours:
-        mask = np.zeros(img_filtered_contours_copy.shape[:2], dtype="uint8")
+        mask = np.zeros(img.shape[:2], dtype="uint8")
         rbox = cv.minAreaRect(contour)
+        x, y, width, height, angle = get_box_values(rbox)
+
+        if angle < -45.0:
+            angle += 90.0
+
         pts = cv.boxPoints(rbox).astype(np.int32)
         cv.drawContours(mask, [pts], -1, 1, -1)
-        target = cv.bitwise_and(img_filtered_contours_copy, img_filtered_contours_copy, mask=mask)
-
-        cv.imshow('Target', target)
+        img_target = cv.bitwise_and(img_filtered_contours, img_filtered_contours, mask=mask)
+        img_target = rotate_image(img_target, angle)
+        cv.imshow('Final target', img_target)
         cv.waitKey(0)
+    print()
